d4079b73761b6cf7bfe7d68d24a6c0a54ef3adfa
Harry Zoeftig <harry.zoeftig@btinternet.com>
2026-03-17 15:40:23 +0000

    vector knn, added abs error, save error and results to CSVs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from dataset import Dataset, load_csv
+import time
 
 def knn_factory(k, training : Dataset, distance_func):
     def knn(x_new):
@@ -20,7 +21,22 @@ def knn_factory(k, training : Dataset, distance_func):
         for (x, y, d) in closest:
             sum_y += y
         return sum_y / len(closest)
-    return knn
+
+    def knn_vector(x_new):
+        dist_vector = distance_func(training.data, x_new)
+        combo_vec = np.c_[(np.c_[training.data, training.labels]), dist_vector]
+        combo_vec = combo_vec[combo_vec[:, -1].argsort()]
+        closest = combo_vec[:k, :]
+        return np.sum(closest[:, -2]) / closest.shape[0]
+
+    return knn_vector
+
+def vector_euclidean_distance(vector1, new_x):
+    minus = vector1 - new_x
+    square = np.square(minus)
+    sum_vec = np.sum(square, axis=1)
+    sqrt_vec = np.sqrt(sum_vec)
+    return sqrt_vec
 
 def conn(configs, dataset, budget, k, size_eval, evaluation_func):
     np.random.shuffle(configs)
@@ -30,7 +46,7 @@ def conn(configs, dataset, budget, k, size_eval, evaluation_func):
         part1 = [i[1] for i in eval_configs_list]
         part2 = [j[0] for j in eval_configs_list]
         eval_configs : Dataset = Dataset(np.array(part1).squeeze(), np.array(part2).squeeze())
-        model = knn_factory(k, eval_configs, (lambda q, p : np.sqrt(np.sum((q - p)**2))))
+        model = knn_factory(k, eval_configs, vector_euclidean_distance)
         predict_xs = []
         for l in range(0, len(remainder_configs)):
             predict_xs.append((l, model(np.array(remainder_configs[l]))))
@@ -52,11 +68,23 @@ def measure(dataset, x):
     return dataset.data[find_x], dataset.labels[find_x]
 
 def main(directory, search_function, **params):
+    start = time.time()
     files = os.listdir(directory)
     for filename in files:
         data: Dataset = load_csv(filename, directory, dtype=float)
         best_complexity = search_function(data.data.copy(), data, **params)
-        print(f"System: {filename}\n\tBest solution:\t\t{best_complexity[0].tolist()}\n\tBest performance:\t{best_complexity[1].item()}\n")
+        print(f"System: {filename}\n" +
+              f"\tBest solution:\t\t{best_complexity[0].squeeze().tolist()}\n" +
+              f"\tBest performance:\t{best_complexity[1].item()}")
+        real_best_complexity = np.min(data.labels)
+        abs_error = np.abs(best_complexity[1] - real_best_complexity)
+        with open(f"./error/{filename.split('.')[0]}_error.csv", "a") as f:
+            f.write(f"{params['k']},{abs_error.item()}\n")
+        with open(f'./results/{filename.split(".")[0]}_results.csv', 'a') as f:
+            f.write(",".join(best_complexity[0].flatten().astype(str)) + f",{best_complexity[1].item()}\n")
+        print(f"Absolute error: {abs_error.item():.4f}\n")
+    print(f"Total time taken: {(time.time() - start):.2f}s")
 
 if __name__ == "__main__":
-    main("datasets", conn, budget=100-2, k=3, size_eval=2, evaluation_func=min)
+    for count_i in range(0, 10):
+        main("datasets", conn, budget=100-2, k=3, size_eval=2, evaluation_func=min)
