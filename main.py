f00651b23da39961a329acb933237ee0ef33d3ea
harryz8 <harry.zoeftig@btinternet.com>
2026-04-14 16:07:24 +0100

    added tooltips

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import os
 from configuration_tuning import tune_configuration_for
 from graph_visualise import plot_results, plot_hyperparameters
 
-conversion = {
-    "Budget": "budget",
-    "Evaluation size": "size_eval",
-    "Hidden layer size": "hidden_size",
-    "Epochs for one batch" : "epochs_1batch",
-    "Epochs for evaluation batch": "epochs",
-    "Learning rate": "learning_rate",
-    "Time taken": "total_time",
-    "Mean Absolute Error": "absolute_error"
-}
+settings_info = {
+    "Budget": {"csv_title": "budget", "description": "The maximum number of times the real performance can be measured."},
+    "Evaluation size": {"csv_title": "size_eval", "description": "This refers to the amount of the budget that is used to create labelled training data for training the initial model."},
+    "Hidden layer size": {"csv_title": "hidden_size", "description": "This refers to the number of artificial neurons in the hidden layer of the neural network (the layer between the input and output layers)."},
+    "Epochs for one batch": {"csv_title": "epochs_1batch", "description": "This refers to the number of times that the backwards pass is run and the weights are updated when updating the model with a new piece of labelled data."},
+    "Epochs for evaluation batch": {"csv_title": "epochs", "description": "This refers to the number of times that the backwards pass is run and the weights are updated when training the initial model."},
+    "Learning rate": {"csv_title": "learning_rate", "description": "This hyper-parameter controls how quickly the neural network model learns from the error of the previous pass"},
+    "Time taken": {"csv_title": "total_time", "description": ""},
+    "Mean Absolute Error": {"csv_title": "absolute_error", "description": ""}
+     }
 
 
 class Window(tk.Tk):
@@ -104,11 +104,12 @@ class HyperparameterGraphSelect(tk.Frame):
         self.x_selected = tk.StringVar()
         for hyperparameter in ["Budget", "Evaluation size", "Hidden layer size", "Epochs for evaluation batch", "Learning rate", "Epochs for one batch"]:
             hyperparameter_radiobuttons.append(
-                tk.Radiobutton(hyperparameter_frame, text=hyperparameter, variable=self.x_selected, value=conversion[hyperparameter],
+                tk.Radiobutton(hyperparameter_frame, text=hyperparameter, variable=self.x_selected, value=settings_info[hyperparameter]['csv_title'],
                                indicatoron=False, width=20, padx=5, pady=5)
             )
             hyperparameter_radiobuttons[-1].pack(side="left")
-
+            self.master.update()
+            ToolTip(self, hyperparameter_radiobuttons[-1], settings_info[hyperparameter]['description'])
         y_axis_label = tk.Label(self, text="Y axis:")
         y_axis_label.pack()
         metric_frame = tk.Frame(self)
@@ -117,7 +118,7 @@ class HyperparameterGraphSelect(tk.Frame):
         self.y_selected = tk.StringVar()
         for metric in ["Time taken", "Mean Absolute Error"]:
             metric_radiobuttons.append(
-                tk.Radiobutton(metric_frame, text=metric, variable=self.y_selected, value=conversion[metric],
+                tk.Radiobutton(metric_frame, text=metric, variable=self.y_selected, value=settings_info[metric]['csv_title'],
                                indicatoron=False, width=20, padx=5, pady=5)
             )
             metric_radiobuttons[-1].pack(side="left")
@@ -131,6 +132,52 @@ class HyperparameterGraphSelect(tk.Frame):
             return
         plot_hyperparameters(self.master.chosen_dataset.get(), self.master.budget.get(), self.x_selected.get(), self.y_selected.get())
 
+#class based on https://stackoverflow.com/questions/3221956/how-do-i-display-tooltips-in-tkinter
+class ToolTip:
+    def __init__(self, parent, widget, text):
+        self.widget = widget
+        self.text = text
+        self.parent = parent
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.waiter = None
+        self.toplevel_window = None
+
+    def widget_after(self, event=None):
+        self.waiter = self.widget.after(100, lambda:self.show(event))
+
+    def show(self, event=None):
+        self.toplevel_window = tk.Toplevel(self.widget)
+        self.toplevel_window.wm_overrideredirect(True)
+        x, y, _, _ = self.widget.bbox("insert")
+        lb = tk.Label(self.toplevel_window, text=self.text, wraplength=200)
+        lb.config(bg="white", padx=5, pady=5, borderwidth=1, relief="solid", font=("Arial", 8))
+        lb.pack()
+        if event:
+            self.toplevel_window.wm_geometry(
+                f"+{self.parent.master.winfo_x() + self.parent.winfo_x() + self.widget.winfo_x() + event.x + 5}" +
+                f"+{self.parent.master.winfo_y() + self.parent.winfo_y() + self.widget.winfo_y() + event.y + 5}")
+        else:
+            self.toplevel_window.wm_geometry(
+                f"+" +
+                f"{self.parent.master.winfo_x() + self.parent.winfo_x() + self.widget.winfo_x() + self.widget.winfo_width()}" +
+                f"+{self.parent.master.winfo_x() + self.parent.winfo_x() + self.widget.winfo_x() + self.widget.winfo_height() + 5}"
+            )
+
+    def hide(self):
+        if self.toplevel_window:
+            self.toplevel_window.destroy()
+            self.toplevel_window = None
+
+    def enter(self, event):
+        if (self.waiter is None) and (self.toplevel_window is None):
+            self.widget_after(event)
+
+    def leave(self, event=None):
+        if self.waiter:
+            self.widget.after_cancel(self.waiter)
+        self.waiter = None
+        self.hide()
 
 if __name__ == '__main__':
     window = Window()
